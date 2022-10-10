6c34d442b044e5b8a9d55ef628ead5719e89d31a
Jhonn7u7 <isra6330@gmail.com>
2022-10-10 18:32:48 -0500

    first commit

---
+++ components/Categories.vue
@@ -0,0 +1,172 @@
+<template>
+    <h2 class="text-center font-bold">Caballero</h2>
+    <div class="flex flex-col w-full flex-nowrap lg:flex-row "> <!-- conteiner -->
+  <div class="card w-96 bg-base-100 shadow-xl  m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+ <div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+</div> <!-- conteiner end -->
+<div class="flex flex-col w-full flex-nowrap lg:flex-row "> <!-- conteiner -->
+  <div class="card w-96 bg-base-100 shadow-xl  m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+ <div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+</div> <!-- conteiner end -->
+<Hero/>
+    <div class="flex flex-col w-full flex-nowrap lg:flex-row "> <!-- conteiner -->
+  <div class="card w-96 bg-base-100 shadow-xl  m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+ <div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+</div> <!-- conteiner end -->
+<div class="flex flex-col w-full flex-nowrap lg:flex-row "> <!-- conteiner -->
+  <div class="card w-96 bg-base-100 shadow-xl  m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+ <div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+<div class="card w-96 bg-base-100 shadow-xl m-4 image-full"><!-- tarjeta de categoria -->
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Jeans</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> <!-- final de tarje de categoria -->
+</div> <!-- conteiner end -->
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLayout>
+    <Carousel/>
+    <Info/>
+    <Categories/>
+    <Hero/>
+  </NuxtLayout>
+</template>
